144e06e5414bff9ac3f7d889df53a6d80fe0c772
Jainy <jainyestefany@gmail.com>
2022-03-16 20:18:17 -0300

    Esqueleto

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <AppTopBar/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import AppTopBar from './components/shared/AppTopBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppTopBar,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/views/ListaPessoas.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <h2>Pessoas</h2>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'ListaPessoas'
+}
+</script>
+
+<style scoped>
+
+</style>
