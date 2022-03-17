aae551599592b4418b85759d68d05064df813155
Jainy <jainyestefany@gmail.com>
2022-03-16 22:21:59 -0300

    Página Pessoas, Localização e Implementação API

--- src/views/ListaPessoas.vue
+++ src/views/ListaPessoas.vue
@@ -1,15 +1,44 @@
 <template>
     <v-container>
         <h2>Pessoas</h2>
+         <v-img class="localizacao1-imagem mb-4" src="https://www.pikpng.com/pngl/b/295-2958164_group-of-people-image-people-cartoon-transparent-background.png" alt="">
+        </v-img>
+
+        <p>João da Silva</p>
+        <div class="flexbox">
+        <v-img class="localizacao2-imagem mb-2" src="https://i.pinimg.com/474x/2f/01/d2/2f01d2c2c69f01fdd1bf77b332e21971.jpg" alt="">
+        </v-img>
+
+        <v-btn id="botao1">
+            <router-link to="/LocalizacaoPessoas">Minha Localização</router-link>
+        </v-btn>
+        </div>
+
     </v-container>
 </template>
 
 <script>
 export default {
-    name: 'ListaPessoas'
+    name: 'ListaPessoas',
 }
 </script>
 
 <style scoped>
+    .flexbox{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    .localizacao2-imagem{
+        width: 200px;
+        height: 200px;
+    }
+
+    #botao1{
+        background-color: #81D4FA;
+    }
+
 
 </style>
--- src/views/LocalizacaoPessoas.vue
+++ src/views/LocalizacaoPessoas.vue
@@ -1,12 +1,34 @@
 <template>
     <v-container>
         <h2>Localização</h2>
+        <span v-for="localizacao of localizacao" :key="localizacao.country">
+        </span>
+        <p>País: {{localizacao.country}}</p>
+        <p>Código Postal: {{localizacao.postcode}}</p>
+        <p>País: {{localizacao.places}}</p>
+        
     </v-container>
 </template>
 
 <script>
 export default {
-    name: 'LocalizacaoPessoas'
+    name: 'LocalizacaoPessoas',
+    
+        data() {
+        return  {
+            localizacao: []
+        }
+    },
+
+    created() {
+        fetch('https://api.zippopotam.us/us/33162')
+           .then(resposta => resposta.json())
+           .then(json => {
+               this.localizacao = json
+                console.log(this.localizacao)
+           });
+
+    }
 }
 </script>
     
