0bb19468615966e251a278fd4e21aaa4103fc687
Sóstenes Rodrigues <sostenesrs@gmail.com>
2022-03-18 15:15:57 -0300

    commit teste nova na nova máquina

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -6,7 +6,7 @@
 
         <p>O número de pessoas desaparecidas no Brasil cresce a cada ano, embora o país não tenha como precisar este quantitativo. Para suprir essa necessidade, deve ser criado um Cadastro Nacional de Pessoas Desaparecidas. </p>
         <p>De acordo com o levantamento do Fórum Brasileiro de Segurança Pública, há uma estimativa de que, de 2006 a 2016, cerca de 695 mil pessoas tenham sido dadas como desaparecidas nos registros policiais. Os dados do 14º Anuário da Segurança Pública 2020 apontam que, em 2018, foram recebidas quase 78 mil notificações de pessoas desaparecidas e cerca de 79 mil em 2019.</p>
-        
+        <p> Teste alteração </p>
     
     </v-container>
 </template>
